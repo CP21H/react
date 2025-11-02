d49773bd537d6dc44300c07545eed0097c46ba18
Cristhian Prado <135445150+CP21H@users.noreply.github.com>
2025-11-02 09:19:27 -0500

    Handling complex state

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -295,6 +295,7 @@ const App = () => {
 //
 //==============================================
 
+/*
 const Display = ( {counter}) => {
   return (
     <div>{counter}</div>
@@ -331,6 +332,76 @@ const App = () => {
         onClick = {decreaseByOne}
         text = 'minus'
       />
+    </div>
+  )
+}
+*/
+
+//==============================================
+//
+// Part 1d: Complex state, debugging React Apps
+// Sub-section 1: Complex state
+//
+// Notes: - For handling more complex state, we can use useState() multiple times
+//        - OBJECT SPREAD SYNTAX: Adding ...objectName copies over properties of the object
+//          then we can specify the particular ones we want to change
+//        - REACT CONVENTION: It is forbidden to mutate state directly, ex: clicks.left++
+//
+//==============================================
+
+/*
+const App = () => {
+  const [clicks, setClicks] = useState({
+    left: 0, right: 0
+  })
+
+  const handleLeftClick = () => 
+    setClicks({ ...clicks, left: clicks.left + 1})
+
+  const handleRightClick = () => 
+    setClicks({ ...clicks, right: clicks.right + 1})
+
+  return (
+    <div>
+      {clicks.left}
+      <button onClick={handleLeftClick}>left</button>
+      <button onClick={handleRightClick}>right</button>
+      {clicks.right}
+    </div>
+  )
+}
+*/  
+
+//==============================================
+//
+// Sub-section 2: Handling arrays
+//
+// Notes: - 
+//
+//==============================================
+
+const App = () => {
+  const [left, setLeft] = useState(0)
+  const [right, setRight] = useState(0)
+  const [allClicks, setAll] = useState([])
+
+  const handleLeftClick = () => {
+    setAll(allClicks.concat('L'))
+    setLeft(left + 1)
+  }
+
+  const handleRightClick = () => {
+    setAll(allClicks.concat('R'))
+    setRight(right + 1)
+  }
+
+  return (
+    <div>
+      {left}
+      <button onClick={handleLeftClick}>left</button>
+      <button onClick={handleRightClick}>right</button>
+      {right}
+      <p>{allClicks.join(' ')}</p>
     </div>
   )
 }
