75d5944ab756041f346f5b6c7151ceac7ff4e8f3
Cristhian Prado <135445150+CP21H@users.noreply.github.com>
2025-12-05 21:07:47 -0500

    Part 2b, Exercise 2.6 and 2.7

--- part2/src/App.jsx
+++ part2/src/App.jsx
@@ -150,6 +150,7 @@ const App = (props) => {
 //
 //==============================================
 
+/*
 import { useState } from 'react'
 import Note from "./components/Note"
 
@@ -207,6 +208,69 @@ const App = (props) => {
     </>
   )
 }
+*/
 
+//==============================================
+//
+// Sub-section 2.6->: Exercises
+//
+// Notes: - 
+
+//==============================================
+
+import { useState } from 'react'
+
+const App = () => {
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas' }
+  ]) 
+  const [newName, setNewName] = useState('')
+
+  const handleNameChange = (event) => {
+    setNewName(event.target.value)
+  }
+
+  const updatePhonebook = (event) => {
+    event.preventDefault()
+    const flag = true
+    
+    persons.forEach(person => {
+      if (person.name == newName) {
+        alert(`${newName} is in the phonebook`)
+        flag = false
+      }
+    })
+
+    if (flag == true) {
+      const personObject = {
+        id: String(persons.length + 1),
+        name: newName,
+      }
+
+      setPersons(persons.concat(personObject))
+      setNewName('')
+    }
+  }
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form>
+        <div>
+          name: <input value={newName} onChange={handleNameChange}/>
+        </div>
+        <div>
+          <button onClick={updatePhonebook} type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      <ul>
+        {persons.map(person => 
+          <li>{person.name}</li>
+        )}
+      </ul>
+    </div>
+  )
+}
 
 export default App
